98e862af0fd540aca92fc5dace5f21d40b756601
Vladyslav V <v.vasyliev@quantal.solutions>
2020-09-02 18:51:56 +0300

    Update vptree_factory.dart

--- lib/vptree_factory.dart
+++ lib/vptree_factory.dart
@@ -1,6 +1,9 @@
+import './vptree.dart';
+
 class VPTreeFactory {
   build(List<dynamic> element, int bucketSize, computeDistanceCallback) {
     var bucketSize = 0, vptree, vptreeb, vptree2, vptreeb2;
+    return VpTree();
   }
 
   load(List<dynamic> element, int bucketSize, computeDistanceCallback) {
