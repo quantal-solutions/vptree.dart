e9e3520e069880530b47a56db71e83ed0c0beb92
Vladyslav V <v.vasyliev@quantal.solutions>
2020-09-03 16:32:14 +0300

    Update vptree_select_test.dart

--- test/vptree_select_test.dart
+++ test/vptree_select_test.dart
@@ -39,92 +39,190 @@ void main() {
         }
     }
 
-  test01() {
-	test_set(size) {
-		List v;
-		for (var i = 0; i < size; i += 4) {
-			v.add(i / 2);
-			v.add((size - 2) - (i / 2));
-		}
-		for (var i = 1; i < size; i += 2)
-			v.add(i);
-		return v;
-	}
-
-	do_test01(size) {
-		var set = test_set(size);
-		var s = set.slice().sort( Function(a, b) { return a-b; });
-		for (var j = 0; j < size; ++j) {
-			var v = set.slice();
-			vPTreeFactory.select(v, j, infComparator);
-
-			if( v[j] == s[j] ){
-        return true;
+    expect(1, 1);
+  });
+  test('GCC Standard Library nth_element test suite 02', () {
+    VPTreeFactory vPTreeFactory;
+    dynamic infComparator;
+    test01() {
+      test_set(size) {
+        List v;
+        for (var i = 0; i < size; i += 4) {
+          v.add(i / 2);
+          v.add((size - 2) - (i / 2));
+        }
+        for (var i = 1; i < size; i += 2) v.add(i);
+        return v;
       }
+      
+      do_test01(size) {
+      	var set = test_set(size);
+      	var s = set.sort( function(a, b) { return a-b; });
+      	for (var j = 0; j < size; ++j) {
+      		var v = set;
+      		vPTreeFactory.select(v, j, infComparator);
 
-			for (var i = 0; i < j; ++i)
-				if( !(v[j] < v[i]) ){
-          return true;
-        }
+      		if( v[j] == s[j] ){
+            return true;
+          }
 
-			for (var i = j; i < v.length; ++i)
-				if( !(v[i] < v[j]) ){
-          return true;
-        }
-		}
-	}
+      		for (var i = 0; i < j; ++i)
+      			if( !(v[j] < v[i]) ){
+              return true;
+            }
+
+      		for (var i = j; i < v.length; ++i)
+      			if( !(v[i] < v[j]) ){
+              return true;
+            }
+      	}
+      }
 
 	var mAX_SIZE = (1 << 10);
 	mAX_SIZE = 256;
 	for (var size = 4; size <= mAX_SIZE; size <<= 1)
 		do_test01(size);
-}
+    }
 
-test05() {
-	var A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-	var B = [10, 20, 1, 11, 2, 12, 3, 13, 4, 14, 5, 15, 6, 16, 7, 17, 8, 18, 9, 19];
-	var C = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
-	var N = A.length;	
-	var logN = 3;	
-	var P = 7;
+    expect(1, 1);
+  });
 
-	CompLast(x, y) { return x % 10 < y % 10; }
+  test('GCC Standard Library nth_element test suite 03', () {
+    VPTreeFactory vPTreeFactory;
+    dynamic infComparator;
+    test05() {
+      var A = [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15,
+        16,
+        17,
+        18,
+        19,
+        20
+      ];
+      var B = [
+        10,
+        20,
+        1,
+        11,
+        2,
+        12,
+        3,
+        13,
+        4,
+        14,
+        5,
+        15,
+        6,
+        16,
+        7,
+        17,
+        8,
+        18,
+        9,
+        19
+      ];
+      var C = [
+        20,
+        19,
+        18,
+        17,
+        16,
+        15,
+        14,
+        13,
+        12,
+        11,
+        10,
+        9,
+        8,
+        7,
+        6,
+        5,
+        4,
+        3,
+        2,
+        1
+      ];
+      var N = A.length;
+      var logN = 3;
+      var P = 7;
 
-	var s1 = B.slice();
+      CompLast(x, y) {
+        return x % 10 < y % 10;
+      }
 
-	dynamic pn = (N / 2) - 1;	
-	vPTreeFactory.select(s1, pn, infComparator);
-	for(var i = pn; i < N; ++i) if( !(s1[i] < s1[pn]) ){ return true;}
+      var s1 = B;
 
-	vPTreeFactory.select(s1, pn, CompLast);
-	for(var i = pn; i < N; ++i) if( !CompLast(s1[i], s1[pn]) ){ return true;}
-}
+      dynamic pn = (N / 2) - 1;
+      vPTreeFactory.select(s1, pn, infComparator);
+      for (var i = pn; i < N; ++i)
+        if (!(s1[i] < s1[pn])) {
+          return true;
+        }
 
+      vPTreeFactory.select(s1, pn, CompLast);
+      for (var i = pn; i < N; ++i)
+        if (!CompLast(s1[i], s1[pn])) {
+          return true;
+        }
+    }
 
-MSDNTest() {
-	var list = [4, 10, 70, 30, 10, 69, 96, 100];
-	var pivot = vPTreeFactory.select(list, 3, infComparator);
-	if(pivot == 30){
-    return true;
-  }
-	for(var i = 0; i < 3; ++i) if( list[i] < 30 ){return true;}
-	if(list[3] == 30){
-    return true;
-  }
-	for(var i = 4; i < 8; ++i) if( 30 < list[i] ){ return true;}	
-}
+    expect(1, 1);
+  });
 
+  test('GCC Standard Library nth_element test suite 04', () {
+    VPTreeFactory vPTreeFactory;
+    dynamic infComparator;
 
-singleElementTest() {
-	var list = [5];
-	var pivot = vPTreeFactory.select(list, 0, infComparator);
-	if(pivot == 5){
-    return true;
-  }
-	if(list == [5]){
-    return true;
-  }
-}
+    MSDNTest() {
+      var list = [4, 10, 70, 30, 10, 69, 96, 100];
+      var pivot = vPTreeFactory.select(list, 3, infComparator);
+      if (pivot == 30) {
+        return true;
+      }
+      for (var i = 0; i < 3; ++i)
+        if (list[i] < 30) {
+          return true;
+        }
+      if (list[3] == 30) {
+        return true;
+      }
+      for (var i = 4; i < 8; ++i)
+        if (30 < list[i]) {
+          return true;
+        }
+    }
+
+    expect(1, 1);
+  });
+
+  test('GCC Standard Library nth_element test suite 05', () {
+    VPTreeFactory vPTreeFactory;
+    dynamic infComparator;
+    singleElementTest() {
+      var list = [5];
+      var pivot = vPTreeFactory.select(list, 0, infComparator);
+      if (pivot == 5) {
+        return true;
+      }
+      if (list == [5]) {
+        return true;
+      }
+    }
 
     expect(1, 1);
   });
