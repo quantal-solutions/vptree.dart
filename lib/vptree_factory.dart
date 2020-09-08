5cef6c875bbdf29bcae3b35cba07f4eecbcc287d
Vladyslav V <v.vasyliev@quantal.solutions>
2020-09-08 11:47:52 +0300

    add new method stringify

--- lib/vptree_factory.dart
+++ lib/vptree_factory.dart
@@ -14,38 +14,4 @@ class VpTreeFactory {
 
   select(List array, int k, Function(int, int) infComparator) {}
 
-  stringify(root, tree){
-    var stack = [root || tree], s = '';
-		while (stack.length) {
-			var node = stack.pop();
-
-			if (node.length) return '['+node.join(',')+']';
-
-			s += '{i:' + node.i;
-			if (node.hasOwnProperty('m')) {
-				s += ',m:' + node.m + ',M:' + node.M + ',μ:' + node.μ;
-			}
-			if (node.hasOwnProperty('b')) {
-				s += ',b:[' + node.b + ']';
-			}
-			if (node.hasOwnProperty('L')) {
-				var L = node.L;
-				if (L) {
-					s += ',L:';
-					if (L.length) s += '[' + L + ']';
-					else s += stringify(L);
-				}
-			}
-			if (node.hasOwnProperty('R')) {
-				var R = node.R;
-				if (R) {
-					s += ',R:';
-					if (R.length) s += '[' + R + ']';
-					else s += stringify(R);
-				}
-			}
-			s += '}';
-		}
-		return "";
-  }
 }
