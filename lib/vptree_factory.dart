d3cc7a3ba4e9216988f3fb3772e606283f83e82e
Vladyslav V <v.vasyliev@quantal.solutions>
2020-09-10 13:02:01 +0300

    Update vptree_factory.dart

--- lib/vptree_factory.dart
+++ lib/vptree_factory.dart
@@ -1,67 +1,76 @@
 import './vptree.dart';
 
-
 class VpTreeFactory {
-  build(List<List<int>> element, int bucketSize,
+  build(List<List<int>> elements, int bucketSize,
       Function(List<int>, List<int>) computeDistanceCallback) {
-    var bucketSize = 0, vptree, vptreeb, vptree2, vptreeb2;
-
     var list = [];
-    for (var i = 0, n = element.length; i < n; i++) {
-      list[i] = {i: i};
+    for (var i = 0, n = elements.length; i < n; i++) {
+      list[i] = {"i": i};
     }
-    var tree = recurseVPTree(element, list, bucketSize, computeDistanceCallback);
+    var tree =
+        recurseVPTree(elements, list, bucketSize, computeDistanceCallback);
     return new VpTree();
   }
 
-	recurseVPTree(List<List<int>> elements, List<Map<String, int>> list, 
-  int bucketSize, Function(List<int>,List<int>) computeDistanceCallback) {
-		if (list.length == 0) return null;
-		var i;
-		var listLength = list.length;
-		if (bucketSize > 0 && listLength <= bucketSize) {
-			var bucket = [];
-			for (i = 0; i < listLength; i++) {
-				bucket[i] = list[i].i;
-			}
-			return bucket;
-		}
-		var vpIndex = selectVPIndex(list),
-		node = list[vpIndex];
-		list.addAll("vpIndex", 1);
-		listLength--;
-		node = { i: node.i };
-		if (listLength == 0) { 
-      return node;
+  recurseVPTree(List<List<int>> elements, 
+  List<Map<String, int>> list,
+  int bucketSize, 
+  Function(List<int>, List<int>) computeDistanceCallback) {
+    if (list.length == 0) {
+      return null;
+    }
+    var i;
+    var listLength = list.length;
+    if (bucketSize > 0 && listLength <= bucketSize) {
+      var bucket = [];
+      for (i = 0; i < listLength; i++) {
+        bucket[i] = list[i]["i"];
       }
-    var vp = elements[node.i];
-    var dmin.Infinity();
-    var dmax = 0,
-    item, dist, n;
-		for (i = 0, n = listLength; i < n; i++) {
-			item = list[i];
-			dist = distance(vp, elements[item.i]);
-			item.dist = dist;
-			if (dmin > dist) dmin = dist;
-			if (dmax < dist) dmax = dist;
-		}
-		node.m = dmin;
-		node.M = dmax;
+      return bucket;
+    }
+    var vpIndex = selectVPIndex(list), node = list[vpIndex];
+    list.removeAt(vpIndex);
+    listLength--;
+    node = {i: node["i"]};
+    if (listLength == 0) {
+      return node;
+    }
+    var vp = elements[node["i"]];
+    var dmin = double.infinity.toInt();
+    var dmax = 0;
+    var item = 0;
+    var dist = 0;
+    var n = listLength;
+    for (i = 0; i < n; i++) {
+      Map<String, int> item;
+      var dist = computeDistanceCallback(vp, elements[item["i"]]);
+      item["dist"] = dist;
+      if (dmin > dist) dmin = dist;
+      if (dmax < dist) dmax = dist;
+    }
+    node["min"] = dmin;
+    node["max"] = dmax;
 
-		var medianIndex = listLength >> 1,
-		median = select(list, medianIndex, infComparator);
-		var leftItems = list.remove(medianIndex, 0);
-		var rightItems = list;
-		node.mu = median.dist;
-		node.L = recurseVPTree(elements, leftItems, bucketSize, computeDistanceCallback);
-		node.R = recurseVPTree(elements, rightItems, bucketSize, computeDistanceCallback);
-		return node;
-	}
+    var medianIndex = listLength >> 1,
+        median = select(list, medianIndex, distanceComparator);
+    var leftItems = List<Map<String, int>>.from(list);
+    leftItems.removeRange(medianIndex, leftItems.length);
+    list.removeRange(0, medianIndex);
+    var rightItems = list;
+    node["mu"] = median.dist;
+    node["L"] = recurseVPTree(elements, leftItems, bucketSize, computeDistanceCallback);
+    node["R"] = recurseVPTree(elements, rightItems, bucketSize, computeDistanceCallback);
+    return node;
+  }
+
+  distanceComparator(a, b) {
+    return a["dist"] < b["dist"];
+  }
 
   selectVPIndex(list) {
     var math;
-		return math.floor(math.random() * list.length);
-	}
+    return math.floor(math.random() * list.length);
+  }
 
   load(List<List<int>> element, String stringifiedTree,
       Function(List<int>, List<int>) computeDistanceCallback) {
