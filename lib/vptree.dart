9135f00b6e29284aa8f81719d946044dbc880330
Vladyslav V <v.vasyliev@quantal.solutions>
2020-09-11 12:11:35 +0300

    Update vptree.dart

--- lib/vptree.dart
+++ lib/vptree.dart
@@ -1,54 +1,52 @@
 class VpTree {
-List<List<int>> elements;
-Map<String, int> treeNode;
-Function(List<int>, List<int>) computeDistanceCallback;
-int comparisons;
+  List<List<int>> elements;
+  Map<String, int> treeNode;
+  Function(List<int>, List<int>) computeDistanceCallback;
+  int comparisons;
 
-  VpTree(List<List<int>> elements, 
-  Map<String, int> treeNode, 
-  Function(List<int>, List<int>) computeDistanceCallback) {
-		this.elements = elements;
+  VpTree(List<List<int>> elements, Map<String, int> treeNode,
+      Function(List<int>, List<int>) computeDistanceCallback) {
+    this.elements = elements;
     this.treeNode = treeNode;
-		this.computeDistanceCallback = computeDistanceCallback;
-	}
+    this.computeDistanceCallback = computeDistanceCallback;
+  }
 
   search(List<dynamic> element, int bucketSize) {}
-  
+
   stringify() {
-  // var stack = [root || tree], s = '';
-  // while (stack.length) {
-  // 	var node = stack.pop();
+    // var stack = [root || tree], s = '';
+    // while (stack.length) {
+    // 	var node = stack.pop();
 
-  // 	if (node.length) return '['+node.join(',')+']';
+    // 	if (node.length) return '['+node.join(',')+']';
 
-  // 	s += '{i:' + node.i;
-  // 	if (node.hasOwnProperty('m')) {
-  // 		s += ',m:' + node.m + ',M:' + node.M + ',μ:' + node.μ;
-  // 	}
-  // 	if (node.hasOwnProperty('b')) {
-  // 		s += ',b:[' + node.b + ']';
-  // 	}
-  // 	if (node.hasOwnProperty('L')) {
-  // 		var L = node.L;
-  // 		if (L) {
-  // 			s += ',L:';
-  // 			if (L.length) s += '[' + L + ']';
-  // 			else s += stringify(L);
-  // 		}
-  // 	}
-  // 	if (node.hasOwnProperty('R')) {
-  // 		var R = node.R;
-  // 		if (R) {
-  // 			s += ',R:';
-  // 			if (R.length) s += '[' + R + ']';
-  // 			else s += stringify(R);
-  // 		}
-  // 	}
-  // 	s += '}';
-  // }
-		return "";
+    // 	s += '{i:' + node.i;
+    // 	if (node.hasOwnProperty('m')) {
+    // 		s += ',m:' + node.m + ',M:' + node.M + ',μ:' + node.μ;
+    // 	}
+    // 	if (node.hasOwnProperty('b')) {
+    // 		s += ',b:[' + node.b + ']';
+    // 	}
+    // 	if (node.hasOwnProperty('L')) {
+    // 		var L = node.L;
+    // 		if (L) {
+    // 			s += ',L:';
+    // 			if (L.length) s += '[' + L + ']';
+    // 			else s += stringify(L);
+    // 		}
+    // 	}
+    // 	if (node.hasOwnProperty('R')) {
+    // 		var R = node.R;
+    // 		if (R) {
+    // 			s += ',R:';
+    // 			if (R.length) s += '[' + R + ']';
+    // 			else s += stringify(R);
+    // 		}
+    // 	}
+    // 	s += '}';
+    // }
+    return "";
   }
-  Map<String, dynamic> toJson() => {
 
-  };
+  Map<String, dynamic> toJson() => {};
 }
