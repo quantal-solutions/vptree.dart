fe8a5712e3e736b5380277aa4f76d4ade77edff1
Vladyslav V <v.vasyliev@quantal.solutions>
2020-09-16 12:50:35 +0300

    Update priority_queue.dart

--- lib/priority_queue.dart
+++ lib/priority_queue.dart
@@ -27,26 +27,24 @@ class PriorityQueue {
     return -1 - minIndex;
   }
 
-  var api = {
-    // This breaks IE8 compatibility. Who cares ?
-    int length() {
-      return contents.length;
-  };
-    insert(int data, double priority) {
-      var index = binaryIndexOf(priority);
-      if (index < 0) index = -1 - index;
-      if (index < size) {
-        contents.splice(index, 0, {data: data, priority: priority});
-        if (contents.length > size) {
-          contents.length--;
-        }
+  var api = {};
+  int length() {
+    return contents.length;
+  }
+  insert(int data, double priority) {
+    var index = binaryIndexOf(priority);
+    if (index < 0) index = -1 - index;
+    if (index < size) {
+      contents.remove(index, 0, {data: data, priority: priority});
+      if (contents.length > size) {
+        contents.length--;
       }
-      return contents.length === size ? contents[contents.length-1].priority : undefined;
-    },
-
-    List<PriorityQueue.Item> list() {
-      return contents.map(function(item){ return {i: item.data, d: item.priority}; });
     }
-  };
+    return contents.length == size ? contents[contents.length-1].priority : null;
+  }
+
+  List<PriorityQueue.Item> list() {
+    return contents.map(function(item){ return {i: item.data, d: item.priority}; });
+  }
   return api;
 }
