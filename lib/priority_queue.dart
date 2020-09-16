6d02073be99d5cf4724fdc3c7a0ab3d24b07e1f3
Vladyslav V <v.vasyliev@quantal.solutions>
2020-09-16 12:39:10 +0300

    Update priority_queue.dart

--- lib/priority_queue.dart
+++ lib/priority_queue.dart
@@ -1,55 +1,52 @@
 class PriorityQueue {
-  PriorityQueue(int size){
-    size = size;
+  var size = 5;
+  var contents = [];
+
+  PriorityQueue(int size) {
+    this.size = size;
   }
-		var size = 5;
-		var contents = [];
 
-		binaryIndexOf(priority) {
-			var minIndex = 0,
-				maxIndex = contents.length - 1,
-				currentIndex,
-				currentElement;
+  binaryIndexOf(priority) {
+    var minIndex = 0,
+        maxIndex = contents.length - 1,
+        currentIndex,
+        currentElement;
 
-			while (minIndex <= maxIndex) {
-				currentIndex = (minIndex + maxIndex) >> 1;
-				currentElement = contents[currentIndex].priority;
-				 
-				if (currentElement < priority) {
-					minIndex = currentIndex + 1;
-				}
-				else if (currentElement > priority) {
-					maxIndex = currentIndex - 1;
-				}
-				else {
-					return currentIndex;
-				}
-			}
+    while (minIndex <= maxIndex) {
+      currentIndex = (minIndex + maxIndex) >> 1;
+      currentElement = contents[currentIndex].priority;
 
-			return -1 - minIndex;
-		}
+      if (currentElement < priority) {
+        minIndex = currentIndex + 1;
+      } else if (currentElement > priority) {
+        maxIndex = currentIndex - 1;
+      } else {
+        return currentIndex;
+      }
+    }
+    return -1 - minIndex;
+  }
 
-		var api = {
-			// This breaks IE8 compatibility. Who cares ?
-			int length() {
-				return contents.length;
-			},
+  var api = {
+    // This breaks IE8 compatibility. Who cares ?
+    int length() {
+      return contents.length;
+  };
+    insert(int data, double priority) {
+      var index = binaryIndexOf(priority);
+      if (index < 0) index = -1 - index;
+      if (index < size) {
+        contents.splice(index, 0, {data: data, priority: priority});
+        if (contents.length > size) {
+          contents.length--;
+        }
+      }
+      return contents.length === size ? contents[contents.length-1].priority : undefined;
+    },
 
-			insert(int data, double priority) {
-				var index = binaryIndexOf(priority);
-				if (index < 0) index = -1 - index;
-				if (index < size) {
-					contents.splice(index, 0, {data: data, priority: priority});
-					if (contents.length > size) {
-						contents.length--;
-					}
-				}
-				return contents.length === size ? contents[contents.length-1].priority : undefined;
-			},
-
-			List<PriorityQueue.Item> list() {
-				return contents.map(function(item){ return {i: item.data, d: item.priority}; });
-			}
-		};
-		return api;
-	}
+    List<PriorityQueue.Item> list() {
+      return contents.map(function(item){ return {i: item.data, d: item.priority}; });
+    }
+  };
+  return api;
+}
