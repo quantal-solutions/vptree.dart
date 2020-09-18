be9edc3f675aa88c761ac2741877c3f2f16db43c
Vladyslav V <v.vasyliev@quantal.solutions>
2020-09-18 17:09:22 +0300

    Update space_point.dart

--- lib/space_point.dart
+++ lib/space_point.dart
@@ -6,11 +6,13 @@ class SpacePoint {
 
   factory SpacePoint.fromJson(Map<String, dynamic> json) {
     List<dynamic> coords = json['points'];
+    var vpTreeNode = SpacePoint();
     var point = List<SpacePoint>();
     coords.forEach((coordsAll) {
       var coord = SpacePoint.fromJson(coordsAll);
       point.add(coord);
     });
+    return vpTreeNode;
   }
   
   Map<String, dynamic> tpJson() => {
